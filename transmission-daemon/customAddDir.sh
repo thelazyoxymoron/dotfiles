410359b43aa217409ab952f113ba1ac62fa6bddf
Siddhartha <sid@thelazyoxymoron.me>
2021-06-12 19:55:59 +0530

    updated custom addition of files in transmission-daemon failing on whitespace in filename

--- transmission-daemon/customAddDir.sh
+++ transmission-daemon/customAddDir.sh
@@ -5,10 +5,11 @@ graphicsDir="/home/mr_robot/Collection/MAM/Graphic-Novels/"
 orpheusDir="/home/mr_robot/Collection/Orpheus-Downloads/"
 artDir="/home/mr_robot/Collection/Learning-Art/"
 frenchDir="/home/mr_robot/Collection/Learning-Languages/French/"
+cinemazDir="/home/mr_robot/Collection/CinemaZ/"
 
 for file in /home/mr_robot/Downloads/temp/*
 do
-    transmission-remote --add $file -w $artDir
-    rm $file
+    transmission-remote --add "$file" -w $cinemazDir
+    rm "$file"
 done
 
