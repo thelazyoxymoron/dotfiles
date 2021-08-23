c60109da32b331c66ea8b2f3435d1f7826b6403e
Siddhartha <sid@thelazyoxymoron.me>
2021-08-23 16:40:25 +0530

    Added SingleFileZ local mount for making a personal archive

--- scripts/rclone-mount.sh
+++ scripts/rclone-mount.sh
@@ -2,5 +2,9 @@
 if pidof -o %PPID -x "rclone-mount.sh"; then
 exit 1
 fi
-/usr/bin/rclone mount gdrive:Books /home/mr_robot/GoogleDriveBooks/ --vfs-cache-mode writes --daemon --verbose --log-file /home/mr_robot/cronlogs/logseq-mount.log
+/usr/bin/rclone mount gdrive:Books /home/mr_robot/GoogleDriveBooks/ --vfs-cache-mode writes --daemon --verbose --log-file /home/mr_robot/cronlogs/drivebooks-mount.log
+
+/usr/bin/rclone mount gdrive:Backup/logseq /home/mr_robot/logseq-gdrive-backup/ --vfs-cache-mode writes --daemon --verbose --log-file /home/mr_robot/cronlogs/logseq-mount.log
+
+/usr/bin/rclone mount gdrive:Backup/SingleFileZ-Save-Everything /home/mr_robot/Collection/SingleFileZ-Save-Everything/ --vfs-cache-mode writes --daemon --verbose --log-file /home/mr_robot/cronlogs/logseq-mount.log
 exit
